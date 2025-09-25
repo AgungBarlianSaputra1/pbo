0c5264a0a72a9c925c07fb6714afd796e03afa44
AgungBarlianSaputra1 <aliviaabadi007@gmail.com>
2025-09-25 11:32:40 +0700

    Update 03.js

--- 03.js
+++ 03.js
@@ -1,11 +1,37 @@
 class Mahasiswa {
-     constructor(nama, nim, jurusan) {
-    this.nama = nama;  
+  constructor(nama, nim, jurusan) {
+    this.nama = nama;
     this.nim = nim;
     this.jurusan = jurusan;
-    }
+  }
 
-    public void belajar() {
-        conscole.log(nama + " sedang belajar.");
-    }
+  belajar() {
+    console.log(this.nama + " sedang belajar.");
+  }
+
+  info() {
+    console.log(Nama: ${this.nama}, NIM: ${this.nim}, Jurusan: ${this.jurusan});
+  }
 }
+
+class MahasiswaS1 extends Mahasiswa {
+  ujian() {
+    console.log(this.nama + " sedang ujian skripsi.");
+  }
+}
+
+class MahasiswaS2 extends Mahasiswa {
+  tugas() {
+    console.log(this.nama + " sedang mengerjakan tugas penelitian.");
+  }
+}
+
+const mhs1 = new Mahasiswa("Ilqi", "12345", "Informatika");
+const mhs2 = new MahasiswaS1("Budi", "67890", "Sistem Informasi");
+const mhs3 = new MahasiswaS2("Sari", "54321", "Teknik Komputer");
+
+mhs1.belajar();   // Ilqi sedang belajar.
+mhs1.info();      // Nama: Ilqi, NIM: 12345, Jurusan: Informatika
+
+mhs2.ujian();     // Budi sedang ujian skripsi.
+mhs3.tugas();     // Sari sedang mengerjakan tugas penelitian.
